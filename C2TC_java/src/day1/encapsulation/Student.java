f8bc68d2e054f44b8e3e94f3b093c7f0c670776c
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-07-21 15:52:52 +0530

    Addition program

---
+++ C2TC_java/src/day1/encapsulation/Student.java
@@ -0,0 +1,22 @@
+package day1.encapsulation;
+
+public class Student {
+int id;
+private String name;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id=id;
+}
+@Override
+public String toString() {
+	return "Student[id=" +id+",name=" +name+"]";
+}
+}

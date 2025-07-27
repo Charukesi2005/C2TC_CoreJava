6c399a5c7dea116a0e6c2db4bada978c01a1e266
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-07-28 00:20:32 +0530

    Core java

---
+++ C2TC_java/src/com/tnif/daysix/usingfinal/FinalClass.java
@@ -0,0 +1,13 @@
+package com.tnif.daysix.usingfinal;
+
+public class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+
+//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
+class FinalChildClass extends FinalClass {
+	
+
+}

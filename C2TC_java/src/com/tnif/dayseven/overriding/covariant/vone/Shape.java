ea164165e78ebdd6476897f031e6b55222d489a3
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-07-28 23:57:39 +0530

    Core Java

---
+++ C2TC_java/src/com/tnif/dayseven/overriding/covariant/vone/Shape.java
@@ -0,0 +1,13 @@
+//Program to define Base class Shape
+package com.tnif.dayseven.overriding.covariant.vone;
+public class Shape {
+
+	// Member function
+	public void draw() {
+		System.out.println("Drawing a generic shape");
+	}
+
+	public void erase() {
+		System.out.println("Erasing a generic shape");
+	}
+}

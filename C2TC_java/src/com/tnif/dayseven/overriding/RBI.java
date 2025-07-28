41f31c4a47ec254519f770860d20dc9ec8707407
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-07-29 00:09:49 +0530

    Core Java

---
+++ C2TC_java/src/com/tnif/dayseven/overriding/RBI.java
@@ -0,0 +1,9 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package com.tnif.dayseven.overriding;
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}

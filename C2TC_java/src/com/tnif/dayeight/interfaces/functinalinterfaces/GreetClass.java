5970f3dcfed9f501d279574c41971333409427ba
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-07-31 10:10:00 +0530

    Core Java

---
+++ C2TC_java/src/com/tnif/dayeight/interfaces/functinalinterfaces/GreetClass.java
@@ -0,0 +1,10 @@
+//Program to demonstrate FunctionalInterface - Child Class
+package com.tnif.dayeight.interfaces.functinalinterfaces;
+public class GreetClass implements GreetInterface {
+
+	@Override
+	public String greet() {		
+		return "Welcome to the world of Java";
+	}
+
+}
---
+++ C2TC_java/src/com/tnif/dayeight/interfaces/functinalinterfaces/GreetInterface.java
@@ -0,0 +1,6 @@
+//Program to demonstrate FunctionalInterface - Greet Interface
+package com.tnif.dayeight.interfaces.functinalinterfaces;
+@FunctionalInterface
+public interface GreetInterface {
+	public String greet();
+}

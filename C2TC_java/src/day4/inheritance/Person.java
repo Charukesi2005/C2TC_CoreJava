f015b4fae207aa7cacb977dd40d164f4db0fa503
charukesi <charukesi@DESKTOP-7RPVLC5>
2025-08-07 14:48:29 +0530

    Core Java

--- C2TC_java/src/day4/inheritance/Person.java
+++
@@ -1,9 +0,0 @@
-package day4.inheritance;
-
-public class Person {
-private String name;
-private int age;
-void display() {
-	System.out.println("In Person Class");
-}
-}
--- C2TC_java/src/day4/inheritance/Student.java
+++
@@ -1,9 +0,0 @@
-package day4.inheritance;
-
-public class Student extends Person {
-private int sid;
-private String dept;
-void show() {
-	System.out.println("In Student Class");
-}
-}
